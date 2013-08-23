353edf7665628377e3c2be998cf590b8bb742698
Byron Hawkins <byron@hawkinssoftware.net>
2013-08-23 13:00:44 -0700

    Adding a collection util class.

---
+++ src/edu/uci/eecs/crowdsafe/util/CrowdSafeCollections.java
@@ -0,0 +1,17 @@
+package edu.uci.eecs.crowdsafe.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class CrowdSafeCollections {
+
+	public static <T> Collection<T> createSortedCopy(Collection<T> collection,
+			Comparator<T> comparator) {
+		List<T> sorted = new ArrayList<T>(collection);
+		Collections.sort(sorted, comparator);
+		return sorted;
+	}
+}
