731af0419239c5e80011a2f3d6b222498e833374
Byron Hawkins <byron@hawkinssoftware.net>
2014-12-21 21:05:03 -0800

    protoc script and updated test after refactor

--- src/edu/uci/eecs/crowdsafe/common/test/ProtocolBufferTest.java
+++ src/edu/uci/eecs/crowdsafe/common/test/ProtocolBufferTest.java
@@ -5,6 +5,8 @@ import java.io.FileOutputStream;
 
 import com.google.protobuf.CodedOutputStream;
 
+import edu.uci.eecs.crowdsafe.common.data.results.Statistics.IntegerStatistic;
+
 public class ProtocolBufferTest {
 
 	public static void main(String[] args) {
@@ -12,19 +14,17 @@ public class ProtocolBufferTest {
 			File statFile = new File("/home/b/workspace/temp/integer.dat");
 			FileOutputStream out = new FileOutputStream(statFile);
 			CodedOutputStream coded = CodedOutputStream.newInstance(out);
-			
+
 			IntegerStatistic.Builder builder = IntegerStatistic.newBuilder();
 			builder.setId("Count");
 			builder.setName("The sum and total count of something");
 
-//			for (int i = 0; i < 1000; i++) {
-				builder.setValue(17);
-				IntegerStatistic integer = builder.build();
+			builder.setValue(17);
+			IntegerStatistic integer = builder.build();
 
-				int size = integer.getSerializedSize();
-				coded.writeRawLittleEndian32(size);
-				integer.writeTo(coded);
-	//		}
+			int size = integer.getSerializedSize();
+			coded.writeRawLittleEndian32(size);
+			integer.writeTo(coded);
 			coded.flush();
 			out.close();
 		} catch (Throwable t) {
