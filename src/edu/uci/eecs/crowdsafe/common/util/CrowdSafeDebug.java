90650f47cd0f55f00e410f47efbb85dac595befa
Byron Hawkins <byron@hawkinssoftware.net>
2015-11-05 02:22:57 -0800

    report all unreachable entries

--- src/edu/uci/eecs/crowdsafe/common/util/CrowdSafeDebug.java
+++ src/edu/uci/eecs/crowdsafe/common/util/CrowdSafeDebug.java
@@ -3,5 +3,5 @@ package edu.uci.eecs.crowdsafe.common.util;
 public class CrowdSafeDebug {
 
 	public static final boolean LOG_UNREACHABLE_ENTRY_POINTS = true;
-	public static final int MAX_UNREACHABLE_NODE_REPORT = 10;
+	public static final int MAX_UNREACHABLE_NODE_REPORT = 0;
 }
